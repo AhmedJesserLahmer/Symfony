8baf5472a11cf18a6c0c7ace10970157c6443dcb
Ranim <ranimhamrouni@gmail.com>
2026-05-25 16:25:35 +0100

    add product listing and detail pages

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Product;
+
+class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(ProductRepository $repository): Response
+    {
+        $products = $repository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/products/{id}', name: 'app_product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
