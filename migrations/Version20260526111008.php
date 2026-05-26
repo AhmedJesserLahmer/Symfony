ecd9792e34265747c51d862fdfd61a16478a9ca8
Jesser001 <Ahmed.jesser.lahmer@gmail.com>
2026-05-26 12:39:10 +0100

    worked on fixtures

--- migrations/Version20260526111008.php
+++ migrations/Version20260526111008.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260525234829 extends AbstractMigration
+final class Version20260526111008 extends AbstractMigration
 {
     public function getDescription(): string
     {
